9e729b203828155e0a350611cc63ee13107813f2
Mehul <71012892+M-Destiny@users.noreply.github.com>
2022-10-10 06:03:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+//Veritcal menu js
+
+document.getElementById("icon-click").onclick = function(){
+    var menu = document.getElementById('vertical-menu');
+    var record = menu.style.display;
+    if(record == "" || record=="none"){
+        menu.style.display = "block";
+    }
+    else{
+        menu.style.display = "none";
+    }
+}
+// slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
